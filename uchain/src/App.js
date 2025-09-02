7fd6afcea748e4ef850a3c6f1ff1194eadeb14e0
DorisGo <3024365568@qq.com>
2025-09-02 15:56:23 +0800

    调整了部分路径问题

--- uchain/src/App.js
+++ uchain/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/header";
+import Navbar from "./components/Navbar";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Navbar />
+      <main>
+        <h1>欢迎来到我的网站</h1>
+      </main>
+      <Footer />
     </div>
   );
 }
--- uchain/src/components/Navbar..js
+++ uchain/src/components/Navbar..js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Navbar.css"; // 引入样式文件
 
 const Navbar = () => {
   return (
